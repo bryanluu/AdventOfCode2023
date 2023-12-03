1add7879f17e2541c1dc2512ccdbd024a0572902
Bryan Luu <loc.bryan.luu@gmail.com>
2023-12-02 22:16:25 -0800

    :smiley: Added solution for Day 02, Part 2 and updated times.csv

--- Day02/Day02.py
+++ Day02/Day02.py
@@ -4,7 +4,7 @@ import sys
 filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 
 from enum import Enum
-import re
+from functools import reduce
 
 
 class CubeColor(Enum):
@@ -34,24 +34,62 @@ def game_is_valid(game):
     return True
 
 def game_id_number_if_game_is_valid(line):
-    game_id, game = line.strip().split(": ")
+    game_id, game = line.split(": ")
     game_id_number = int(game_id.split(" ")[1])
     return game_id_number if game_is_valid(game) else 0
 
 
-def solve(filename):
-    with open(filename, "r") as file:
-        result = 0
-        for line in file:
-            result += game_id_number_if_game_is_valid(line)
-        print(f"Result: {result}")
+def solve_part_1(lines):
+    result = 0
+    for line in lines:
+        result += game_id_number_if_game_is_valid(line.strip())
+    return result
+
+
+def compute_minimum_cube_set(min_cube_set, bag):
+    for color, count in bag.items():
+        if count > min_cube_set.get(CubeColor(color), 0):
+            min_cube_set[CubeColor(color)] = count
+    return min_cube_set
+
+
+def parse_bag(cube_set_str):
+    bag = {}
+    color_counts = cube_set_str.split(", ")
+    for color_count in color_counts:
+        count, color = color_count.split(" ")
+        bag[CubeColor(color)] = int(count)
+    return bag
+
+
+def power_of_cube_set(line):
+    _, cube_sets = line.split(": ")
+    bags = map(parse_bag, cube_sets.split("; "))
+    minimum_cube_set = reduce(compute_minimum_cube_set, bags)
+    power = 1
+    for color in CubeColor:
+        power *= minimum_cube_set[color]
+    return power
+
+
+def solve_part_2(lines):
+    result = 0
+    for line in lines:
+        result += power_of_cube_set(line.strip())
+    return result
 
 
 if __name__ == "__main__":
     print(f"Input file: {filename}")
     import time
 
-    start = time.time()
-    solve(filename)
-    end = time.time()
-    print(f"Solve time: {end-start} seconds")
+    with open(filename, "r") as file:
+        lines = file.readlines()
+        start = time.time()
+        print(f"Part 1: {solve_part_1(lines)}")
+        end = time.time()
+        print(f"Solve time: {end-start} seconds")
+        start = time.time()
+        print(f"Part 2: {solve_part_2(lines)}")
+        end = time.time()
+        print(f"Solve time: {end-start} seconds")

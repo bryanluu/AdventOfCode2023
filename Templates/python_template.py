54a022a96630000b0b6ab0a1009f5dcce526c6d0
Bryan Luu <loc.bryan.luu@gmail.com>
2023-12-02 22:22:18 -0800

    :wrench: update Python template to include part 2

--- Templates/python_template.py
+++ Templates/python_template.py
@@ -2,13 +2,25 @@
 import sys
 filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 
-def solve(filename):
-  pass # DO STUFF
+def solve_part_1(filename):
+    pass # DO STUFF
+
+def solve_part_2(filename):
+    pass # DO STUFF
 
 if __name__ == '__main__':
-  print(f"Input file: {filename}")
-  import time
-  start = time.time()
-  solve(filename)
-  end = time.time()
-  print(f"Solve time: {end-start} seconds")
+    print(f"Input file: {filename}")
+    import time
+
+    lines = []
+    with open(filename, "r") as file:
+        lines = file.readlines()
+
+    start = time.time()
+    print(f"Part 1: {solve_part_1(lines)}")
+    end = time.time()
+    print(f"Solve time: {end-start} seconds")
+    start = time.time()
+    print(f"Part 2: {solve_part_2(lines)}")
+    end = time.time()
+    print(f"Solve time: {end-start} seconds")

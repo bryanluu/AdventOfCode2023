5ec2f0e573a7790f8258df69e874f4550e6f3089
Bryan Luu <loc.bryan.luu@gmail.com>
2023-12-06 09:05:41 -0800

    :white_check_mark: add test harness

--- Day04/Day04.clj
+++ Day04/Day04.clj
@@ -1,7 +1,9 @@
 #!/usr/bin/env bb
 (ns Day04
   (:require [clojure.string :as str]
-            [clojure.set :as set]))
+            [clojure.set :as set]
+            [clojure.java.io :as io])
+  (:require [clojure.test :refer [deftest is testing run-tests]]))
 
 (def CARD_REGEX #"^Card\s+(?<id>\d+):(?<winningNumbers>(?:\s*\d+\s*)+)\|(?<myNumbers>(?:\s*\d+\s*)+)$")
 
@@ -21,20 +23,32 @@
         _ (re-find m)
         id (Integer/parseInt (.group m "id"))
         winning-numbers (extract-numbers (.group m "winningNumbers"))
-        my-numbers (extract-numbers (.group m "myNumbers"))
-        matches (set/intersection winning-numbers my-numbers)
-        points (case (count matches)
-                 0 0
-                 1 1
-                 (reduce * 1 (repeat (dec (count matches)) 2)))]
+        my-numbers (extract-numbers (.group m "myNumbers"))]
     {:id id
      :winning-numbers winning-numbers
-     :my-numbers my-numbers
-     :points points}))
+     :my-numbers my-numbers}))
+
+(defn power-of-two [n]
+  (reduce * 1 (repeat n 2)))
+
+(defn compute-points
+  "Compute the points for a card."
+  [card]
+  (let [winning-numbers (:winning-numbers card)
+        my-numbers (:my-numbers card)
+        common-numbers (set/intersection winning-numbers my-numbers)
+        points (case (count common-numbers)
+                 0 0
+                 (power-of-two (dec (count common-numbers))))]
+    (assoc card :common common-numbers :points points)))
 
 (defn solve-part-1 [input]
   (let [lines (str/split-lines input)
-        cards (map parse-card lines)]
+        cards (into []
+                    (comp
+                     (map parse-card)
+                     (map compute-points))
+                    lines)]
     (reduce + (map :points cards))))
 
 (comment
@@ -51,8 +65,34 @@
   ;; Do stuff
   )
 
-(let [input-file (or (first *command-line-args*) "input.txt")
-      input (slurp input-file)]
-  (println "Input file: " input-file)
-  (println "Part 1:" (time (solve-part-1 input)))
-  (println "Part 2:" (time (solve-part-2 input))))
+;; Tests
+
+(deftest test-parse-card
+  (is (= {:id 3
+          :winning-numbers #{1 21 53 59 44}
+          :my-numbers #{69 82 63 72 16 21 14 1}}
+         (parse-card "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1"))))
+
+(deftest test-compute-points
+  (is (= {:id 3
+          :winning-numbers #{1 21 53 59 44}
+          :my-numbers #{69 82 63 72 16 21 14 1}
+          :common #{1 21}
+          :points 2}
+         (compute-points {:id 3
+                          :winning-numbers #{1 21 53 59 44}
+                          :my-numbers #{69 82 63 72 16 21 14 1}}))))
+
+(deftest test-solve-part-1
+  (let [input (slurp "example.txt")]
+    (is (= 13 (solve-part-1 input)))))
+
+(when (some->> *command-line-args*
+               first
+               io/as-file
+               #(.exists %))
+  (let [input-file (first *command-line-args*)
+        input (slurp input-file)]
+    (println "Input file: " input-file)
+    (println "Part 1:" (time (solve-part-1 input)))
+    (println "Part 2:" (time (solve-part-2 input)))))

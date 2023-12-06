cbbe3bbe9181e9380e4432f2be90e7fcc8da9ff8
Bryan Luu <loc.bryan.luu@gmail.com>
2023-12-05 21:52:53 -0800

    :pencil: edit template

--- Templates/bb_template.clj
+++ Templates/bb_template.clj
@@ -1,14 +1,15 @@
 #!/usr/bin/env bb
 
-(defn solve-part-1 [input-file]
-  ;; Do stuff
-  ,)
-
-(defn solve-part-2 [input-file]
+(defn solve-part-1 [input]
   ;; Do stuff
   )
 
-(let [input-file (first *command-line-args*)]
+(defn solve-part-2 [input]
+  ;; Do stuff
+  )
+
+(let [input-file (or (first *command-line-args*) "input.txt")
+      input (slurp input-file)]
   (println "Input file: " input-file)
-  (println "Part 1:" (time (solve-part-1 input-file)))
-  (println "Part 2:" (time (solve-part-2 input-file))))
+  (println "Part 1:" (time (solve-part-1 input)))
+  (println "Part 2:" (time (solve-part-2 input))))

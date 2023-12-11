ee317607c928ff2ca0cafc3df3fe9f779c1aafb9
Bryan Luu <loc.bryan.luu@gmail.com>
2023-12-10 20:46:45 -0800

    :smiley: Added solution for Day 05, Part 1 and updated times.csv

--- Day05/Day05.py
+++ Day05/Day05.py
@@ -2,11 +2,48 @@
 import sys
 filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 
-def apply_map(numbers, mapping_lines):
-    pass # DO STUFF
+from functools import reduce
+
+def parse_range_mapping(line: str) -> dict:
+    """Parse a line of the form `dest, src, len` to a range mapping"""
+    dest, src, length = map(int, line.split())
+    return {
+        # inclusive lower bound
+        "lower": src,
+        # non-inclusive upper bound
+        "upper": src + length,
+        # offset
+        "offset": dest - src,
+    }
+
+def apply_map(source_numbers: list, mappings_str: str) -> list:
+    """Apply a mapping given by the mappings_str to the source_numbers."""
+    lines = mappings_str.split("\n")
+    header = lines.pop(0)
+
+    # parse mapping line
+    mapped = {i: i for i in source_numbers}
+    for line in lines:
+        mapping = parse_range_mapping(line)
+        for src in source_numbers:
+            if mapping["lower"] <= src < mapping["upper"]:
+                mapped[src] = src + mapping["offset"]
+    print(header, mapped)
+
+    return [mapped[i] for i in source_numbers]
+
 
 def solve_part_1(lines):
-    pass # DO STUFF
+    # get groups
+    line_groups = "".join(lines).strip().split("\n\n")
+
+    # initialize seeds
+    seeds = list(map(int, line_groups.pop(0).split()[1:]))
+
+    # apply maps to seeds to obtain locations
+    locations = reduce(apply_map, line_groups, seeds)
+
+    return min(locations)
 
 def solve_part_2(lines):
     pass # DO STUFF
--- Day05/test_day5.py
+++ Day05/test_day5.py
@@ -2,9 +2,21 @@ import Day05 as sut
 
 EXAMPLE_FILENAME = "example.txt"
 
+def test_parse_range_mapping():
+    assert sut.parse_range_mapping("50 98 2") == {
+        "lower": 98,
+        "upper": 100,
+        "offset": -48,
+    }
+    assert sut.parse_range_mapping("52 50 48") == {
+        "lower": 50,
+        "upper": 98,
+        "offset": 2,
+    }
+
 def test_apply_map():
     seeds = [79, 14, 55, 13]
-    seed_to_soil = ["seed-to-soil map:\n", "50 98 2\n", "52 50 48\n"]
+    seed_to_soil = "seed-to-soil map:\n50 98 2\n52 50 48"
 
     assert sut.apply_map(seeds, seed_to_soil) == [81, 14, 57, 13]
 

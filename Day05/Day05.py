3407e4c64ac231fb0fca51147dd95e569eda787d
Bryan Luu <loc.bryan.luu@gmail.com>
2023-12-14 08:36:30 -0800

    :test_tube: attempt to solve Day 5 part 2

--- Day05/Day05.py
+++ Day05/Day05.py
@@ -1,8 +1,10 @@
 #!/bin/python3
 import sys
+
 filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 
 from functools import reduce
+
 
 def parse_range_mapping(line: str) -> dict:
     """Parse a line of the form `dest, src, len` to a range mapping"""
@@ -16,7 +18,8 @@ def parse_range_mapping(line: str) -> dict:
         "offset": dest - src,
     }
 
-def apply_map(source_numbers: list, mappings_str: str) -> list:
+
+def apply_map_to_source_numbers(source_numbers: list, mappings_str: str) -> list:
     """Apply a mapping given by the mappings_str to the source_numbers."""
     lines = mappings_str.split("\n")
     header = lines.pop(0)
@@ -41,14 +44,109 @@ def solve_part_1(lines):
     seeds = list(map(int, line_groups.pop(0).split()[1:]))
 
     # apply maps to seeds to obtain locations
-    locations = reduce(apply_map, line_groups, seeds)
+    locations = reduce(apply_map_to_source_numbers, line_groups, seeds)
 
     return min(locations)
 
-def solve_part_2(lines):
-    pass # DO STUFF
 
-if __name__ == '__main__':
+def parse_seed_ranges(line: str) -> list:
+    """Parse a line of the form `seeds: start end start end` to a list of seed ranges"""
+    header, ranges = line.split(":")
+    seed_ranges = {}
+    nums = list(map(int, ranges.split()))
+    while nums:
+        start, length = nums.pop(0), nums.pop(0)
+        seed_ranges[start] = {"start": start, "length": length, "offset": 0}
+
+    return seed_ranges
+
+
+def map_source_range(source_ranges: dict, mapping: dict) -> dict:
+    """Apply a mapping to the source_range"""
+    start = mapping["lower"]
+    end = mapping["upper"]
+    mapped = source_ranges.copy()
+    for src in source_ranges.values():
+        # src starting point
+        src_start = src["start"]
+        # src ending point
+        src_end = src_start + src["length"]
+        # if fully outside of range, don't change range
+        if src_end <= start or src_start >= end:
+            continue
+
+        # if fully inside of range
+        if src_start >= start and src_end <= end:
+            mapped[src_start]["offset"] += mapping["offset"]
+        # elif src_start is inside but src_end is not
+        elif src_start >= start and src_start <= end and src_end > end:
+            mapped[src_start]["length"] = end - src_start
+            mapped[src_start]["offset"] += mapping["offset"]
+            # add the interval after range
+            offset_outside = source_ranges.get(end, {"offset": 0})["offset"]
+            mapped[end] = {
+                "start": end,
+                "offset": offset_outside,
+                "length": src_end - end,
+            }
+        # elif src_end is inside but src_start is not
+        elif src_start < start and src_end >= start and src_end <= end:
+            # trim the interval before range
+            mapped[src_start]["length"] = start - src_start
+            # add the interval inside range
+            mapped[start] = {
+                "start": start,
+                "offset": src["offset"] + mapping["offset"],
+                "length": src_end - start,
+            }
+        # elif range is inside of src
+        elif src_start < start and src_end > end:
+            # trim the interval before range
+            mapped[src_start]["length"] = start - src_start
+            # add the interval inside range
+            mapped[start] = {
+                "start": start,
+                "offset": src["offset"] + mapping["offset"],
+                "length": end - start,
+            }
+            # add the interval after range
+            offset_outside = source_ranges.get(end, {"offset": 0})["offset"]
+            mapped[end] = {
+                "start": end,
+                "offset": offset_outside,
+                "length": src_end - end,
+            }
+    return mapped
+
+
+def apply_map_to_source_ranges(source_ranges: dict, mappings_str: str) -> list:
+    """Apply a mapping given by the mappings_str to the source_ranges."""
+    lines = mappings_str.split("\n")
+    header = lines.pop(0)
+
+    print("src:", source_ranges)
+    for line in lines:
+        mapping = parse_range_mapping(line)
+        mapped = map_source_range(source_ranges, mapping)
+        print(line, mapped)
+
+    return mapped
+
+
+def solve_part_2(lines):
+    # get groups
+    line_groups = "".join(lines).strip().split("\n\n")
+
+    # initialize seeds
+    seed_ranges = line_groups.pop(0)
+
+    # apply maps to seeds to obtain locations
+    locations = reduce(apply_map_to_source_ranges, line_groups, seed_ranges)
+
+    return min(locations)
+
+
+if __name__ == "__main__":
     print(f"Input file: {filename}")
     import time
 
--- Day05/test_day5.py
+++ Day05/test_day5.py
@@ -2,6 +2,7 @@ import Day05 as sut
 
 EXAMPLE_FILENAME = "example.txt"
 
+
 def test_parse_range_mapping():
     assert sut.parse_range_mapping("50 98 2") == {
         "lower": 98,
@@ -14,11 +15,13 @@ def test_parse_range_mapping():
         "offset": 2,
     }
 
-def test_apply_map():
+
+def test_apply_map_to_source_numbers():
     seeds = [79, 14, 55, 13]
     seed_to_soil = "seed-to-soil map:\n50 98 2\n52 50 48"
 
-    assert sut.apply_map(seeds, seed_to_soil) == [81, 14, 57, 13]
+    assert sut.apply_map_to_source_numbers(seeds, seed_to_soil) == [81, 14, 57, 13]
+
 
 def test_part_1():
     lines = []
@@ -26,6 +29,63 @@ def test_part_1():
         lines = file.readlines()
 
     assert sut.solve_part_1(lines) == 35
+
+
+def test_parse_seed_ranges():
+    line = "seeds: 79 14 55 13"
+    assert sut.parse_seed_ranges(line) == {
+        55: {"start": 55, "length": 13, "offset": 0},
+        79: {"start": 79, "length": 14, "offset": 0},
+    }
+
+
+def test_map_source_range():
+    case_A = {
+        4: {"start": 4, "length": 2, "offset": 0},
+        7: {"start": 7, "length": 3, "offset": 0},
+    }
+    case_B = {
+        1: {"start": 1, "length": 4, "offset": 3},
+        5: {"start": 5, "length": 7, "offset": 0},
+    }
+    mapping = {"lower": 6, "upper": 8, "offset": -4}
+
+    assert sut.map_source_range(case_A, mapping) == {
+        4: {"start": 4, "length": 2, "offset": 0},
+        7: {"start": 7, "length": 1, "offset": -4},
+        8: {"start": 8, "length": 2, "offset": 0},
+    }
+    assert sut.map_source_range(case_B, mapping) == {
+        1: {"start": 1, "length": 4, "offset": 3},
+        5: {"start": 5, "length": 1, "offset": 0},
+        6: {"start": 6, "length": 2, "offset": -4},
+        8: {"start": 8, "length": 4, "offset": 0},
+    }
+
+
+def test_apply_map_to_source_ranges():
+    case_A = {
+        4: {"start": 4, "length": 2, "offset": 0},
+        7: {"start": 7, "length": 3, "offset": 0},
+    }
+    case_B = {
+        1: {"start": 1, "length": 4, "offset": 0},
+        5: {"start": 5, "length": 7, "offset": 0},
+    }
+    mapping_line = "seed-to-soil map:\n0 6 4\n8 0 2\n6 2 2"
+
+    assert sut.apply_map_to_source_ranges(case_A, mapping_line) == {
+        4: {"start": 4, "length": 2, "offset": 0},
+        7: {"start": 7, "length": 3, "offset": -6},
+    }
+    assert sut.apply_map_to_source_ranges(case_B, mapping_line) == {
+        1: {"start": 1, "length": 1, "offset": 8},
+        2: {"start": 2, "length": 2, "offset": 4},
+        4: {"start": 4, "length": 2, "offset": 0},
+        6: {"start": 6, "length": 4, "offset": -6},
+        10: {"start": 10, "length": 2, "offset": 0},
+    }
+
 
 def test_part_2():
     lines = []
